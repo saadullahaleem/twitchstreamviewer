ab4272697f5f025e463585902c920cfbd583d2d6
Saadullah Aleem <aleemsaadullah@gmail.com>
2019-02-03 21:12:46 +0500

    updated settings

--- streameventviewer/settings.py
+++ streameventviewer/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-um740_@p94bdodl-nx$rebe5n+39zw(e(pne6=7(0@l^eq6g('
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
